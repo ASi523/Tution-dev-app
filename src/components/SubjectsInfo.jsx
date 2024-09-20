13162dce8f25df185a0ac0fb8b3e9738e220ca05
ASi523 <asiklucky5@gmail.com>
2024-09-20 12:16:43 +0530

    new change

--- src/components/SubjectsInfo.jsx
+++ src/components/SubjectsInfo.jsx
@@ -11,7 +11,7 @@ const MainBox = () => {
             <div className="box-row">
                     <h3>Home Tutors By Category</h3>
                 <ul>
-                        <li>Preschool/</li>
+                        <li>Preschool/School</li>
                         <li>School</li>
                         <li>Arts</li>
                         <li>Science</li>
